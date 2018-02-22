f5943dd6760996006c60fc33c8a0f529eae0578c
Jrsimpson1998 <36742022+Jrsimpson1998@users.noreply.github.com>
2018-02-22 13:09:56 -0500

    llll
    this was added code

--- MaxAreaReactiveBot.java
+++ MaxAreaReactiveBot.java
@@ -1,3 +1,4 @@
 //bot code will go here
 System.out.println("Initial bot position is arbitrary to the performace of the algorithm\n"
 		+ "that is the assumption atleast.");
+System.out.println("This is jackson");
