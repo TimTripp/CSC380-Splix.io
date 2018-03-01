a5ba9f8817813bd6ddc6ff1662f1339d8b350f6e
Timothy Tripp II <tmt5249@uncw.edu>
2018-03-01 11:25:38 -0500

    psudo code
    psudo code comments added

--- MaxAreaReactiveBot.java
+++ MaxAreaReactiveBot.java
@@ -1,4 +1,10 @@
 //bot code will go here
 System.out.println("Initial bot position is arbitrary to the performace of the algorithm\n"
 		+ "that is the assumption atleast.");
-System.out.println("This is jackson");
+/**
+ * --bot starts in 5x5 block moving in a clear direction(no wall in direct path) otherwise random start direction. 
+ * --while(bot tail is not in danger of being hit/crossed by another player, and bot must maintain two blocks closer 
+ * to source blocks then another player to the bots tail)
+ * --bot will move at a constant rate that rate is also same as all players on the board
+ * 
+ */
